c8084f2ef4a4fcdd1fd408caf7436da5a6998c66
David Annex <davidmannex@gmail.com>
2023-02-17 14:07:59 -0500

    i have it working locally

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { stringify } = require('querystring');
 console.log("routes");
 module.exports = app => {
     fs.readFile("db/db.json","utf8", (err, data) => {
@@ -19,21 +20,28 @@ module.exports = app => {
 
     app.post("/api/notes", function(req, res) {
         let newNote = req.body;
-        noteNew.id = notes[notes.length - 1].id + 1;
+        newNote.id = JSON.stringify(parseInt(notes[notes.length - 1].id) + 1);
+        console.log(notes[notes.length - 1].id);
+        console.log(parseInt(notes[notes.length - 1].id)+1);
+        console.log(notes[notes.length - 1]);
+        console.log(newNote);
+
         notes.push(newNote);
         update();
         console.log("Added note");
+        res.json(notes);
         return 
     });
 
     app.get("/api/notes/:id", function(req,res) {
-        res.json(notes[req.params.id]);
+        res.json(notes[req.params.id-1]);
     });
 
     app.delete("/api/notes/:id", function(req, res) {
-        notes.splice(req.params.id, 1);
+        notes.splice(req.params.id-1, 1);
         update();
         console.log("Deleted note");
+        res.json(notes);
     });
     app.get('/notes', function(req,res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
